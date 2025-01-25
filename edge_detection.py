8feda17bc73229ea4932e4070463b767ad8ca176
Sagnic Ghosh <ghoshsagnic04@gmail.com>
2025-01-25 10:39:56 +0530

    Commit 1

--- edge_detection.py
+++ edge_detection.py
@@ -7,10 +7,13 @@ img = cv2.imread(os.path.join('.','data','basketball.png'))
 
 img_edge = cv2.Canny(img, 100, 200)
 
-img_edge_d = cv2.dilate(img_edge, np.ones((5, 5), dtype=np.int8))
+img_edge_d = cv2.dilate(img_edge, np.ones((3, 3), dtype=np.int8))
+
+img_edge_e = cv2.erode(img_edge_d, np.ones((3, 3), dtype=np.int8))
 
 cv2.imshow('img',img)
 cv2.imshow('img_edge',img_edge)
 cv2.imshow('img_edge_d', img_edge_d)
+cv2.imshow('img_edge_e', img_edge_e)
 cv2.waitKey(0)
 
